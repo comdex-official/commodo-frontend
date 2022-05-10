53b16de15f3018df9440ac4db51ae4952b84086d
asif <asif.ansari7774@gmail.com>
2022-05-10 16:27:04 +0530

    details page update ui

--- src/containers/Market/Details/Repay.js
+++ src/containers/Market/Details/Repay.js
@@ -27,81 +27,217 @@ const BorrowTab = (lang) => {
   return (
     <div className="details-wrapper">
       <div className="details-left commodo-card">
-          
+        <div className="assets-select-card mb-4">
+          <div className="assets-left">
+            <label className="leftlabel">
+              Collateral Asset
+            </label>
+            <div className="assets-select-wrapper">
+              <Select
+                className="assets-select"
+                dropdownClassName="asset-select-dropdown"
+                placeholder={
+                  <div className="select-placeholder">
+                    <div className="circle-icon">
+                      <div className="circle-icon-inner" />
+                    </div>
+                    Select
+                  </div>
+                }
+                defaultActiveFirstOption={true}
+                suffixIcon={<SvgIcon name="arrow-down" viewbox="0 0 19.244 10.483" />}
+              >
+                <Option key="1">
+                  <div className="select-inner">
+                    <div className="svg-icon">
+                      <div className="svg-icon-inner">
+                        <SvgIcon name="atom-icon" />
+                      </div>
+                    </div>
+                    <div className="name">Atom</div>
+                  </div>
+                </Option>
+              </Select>
+            </div>
+          </div>
+          <div className="assets-right">
+            <div className="label-right">
+              Available
+              <span className="ml-1">
+                142 USCX
+              </span>
+              <div className="maxhalf">
+                <Button className="active">
+                  Max
+                </Button>
+              </div>
+            </div>
+            <div>
+              <div className="input-select">
+                <Input
+                  placeholder=""
+                  value="23.00"
+                />
+              </div>
+              <small>$120.00</small>
+            </div>
+          </div>
+        </div>
+        <div className="assets-select-card mb-4">
+          <div className="assets-left">
+            <label className="leftlabel">
+              Borrow Asset
+            </label>
+            <div className="assets-select-wrapper">
+              <Select
+                className="assets-select"
+                dropdownClassName="asset-select-dropdown"
+                placeholder={
+                  <div className="select-placeholder">
+                    <div className="circle-icon">
+                      <div className="circle-icon-inner" />
+                    </div>
+                    Select
+                  </div>
+                }
+                defaultActiveFirstOption={true}
+                suffixIcon={<SvgIcon name="arrow-down" viewbox="0 0 19.244 10.483" />}
+              >
+                <Option key="1">
+                  <div className="select-inner">
+                    <div className="svg-icon">
+                      <div className="svg-icon-inner">
+                        <SvgIcon name="atom-icon" />
+                      </div>
+                    </div>
+                    <div className="name">Atom</div>
+                  </div>
+                </Option>
+              </Select>
+            </div>
+          </div>
+          <div className="assets-right">
+            <div>
+              <div className="input-select">
+                <Input
+                  placeholder=""
+                  value="23.00"
+                />
+              </div>
+              <small>$120.00</small>
+            </div>
+          </div>
+        </div>
+        <Row>
+          <Col sm="12" className="mt-3 mx-auto card-bottom-details">
+            <Row className="mt-1">
+              <Col>
+                <label>Current LTV</label>
+              </Col>
+              <Col className="text-right">
+                35%
+              </Col>
+            </Row>
+            <Row className="mt-1">
+              <Col>
+                <label>Liquidation Percentage</label>
+              </Col>
+              <Col className="text-right">
+                10%
+              </Col>
+            </Row>
+            <Row className="mt-1">
+              <Col>
+                <label>Borrow APY</label>
+              </Col>
+              <Col className="text-right">
+                3.80%
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <div className="assets-form-btn">
+          <Button
+            type="primary"
+            className="btn-filled"
+          >
+            Borrow
+          </Button>
+        </div>
       </div>
       <div className="details-right">
-      <div className="commodo-card">
-            <div className="card-head">
-              <div className="head-left">
-                <div className="assets-col">
-                  <div className="assets-icon">
-                    <SvgIcon name="atom-icon" />
-                  </div>
-                  USCX
+        <div className="commodo-card">
+          <div className="card-head">
+            <div className="head-left">
+              <div className="assets-col">
+                <div className="assets-icon">
+                  <SvgIcon name="atom-icon" />
                 </div>
-              </div>
-              <div className="head-right">
-                <span>Oracle Price</span> : $123.45
+                USCX
               </div>
             </div>
-            <List
-              grid={{
-                gutter: 16,
-                xs: 2,
-                sm: 2,
-                md: 3,
-                lg: 2,
-                xl: 2,
-                xxl: 2,
-              }}
-              dataSource={data}
-              renderItem={item => (
-                <List.Item>
-                    <div>
-                      <p>{item.title}</p>
-                      <h3>{item.counts}</h3>
-                    </div>
-                </List.Item>
-              )}
-            />
+            <div className="head-right">
+              <span>Oracle Price</span> : $123.45
+            </div>
           </div>
-          <div className="commodo-card">
-            <div className="card-head">
-              <div className="head-left">
-                <div className="assets-col">
-                  <div className="assets-icon">
-                    <SvgIcon name="atom-icon" />
+          <List
+            grid={{
+              gutter: 16,
+              xs: 2,
+              sm: 2,
+              md: 3,
+              lg: 2,
+              xl: 2,
+              xxl: 2,
+            }}
+            dataSource={data}
+            renderItem={item => (
+              <List.Item>
+                  <div>
+                    <p>{item.title}</p>
+                    <h3>{item.counts}</h3>
                   </div>
-                  USCX
+              </List.Item>
+            )}
+          />
+        </div>
+        <div className="commodo-card">
+          <div className="card-head">
+            <div className="head-left">
+              <div className="assets-col">
+                <div className="assets-icon">
+                  <SvgIcon name="atom-icon" />
                 </div>
-              </div>
-              <div className="head-right">
-                <span>Oracle Price</span> : $123.45
+                USCX
               </div>
             </div>
-            <List
-              grid={{
-                gutter: 16,
-                xs: 2,
-                sm: 2,
-                md: 3,
-                lg: 2,
-                xl: 2,
-                xxl: 2,
-              }}
-              dataSource={data}
-              renderItem={item => (
-                <List.Item>
-                    <div>
-                      <p>{item.title}</p>
-                      <h3>{item.counts}</h3>
-                    </div>
-                </List.Item>
-              )}
-            />
+            <div className="head-right">
+              <span>Oracle Price</span> : $123.45
+            </div>
           </div>
+          <List
+            grid={{
+              gutter: 16,
+              xs: 2,
+              sm: 2,
+              md: 3,
+              lg: 2,
+              xl: 2,
+              xxl: 2,
+            }}
+            dataSource={data}
+            renderItem={item => (
+              <List.Item>
+                  <div>
+                    <p>{item.title}</p>
+                    <h3>{item.counts}</h3>
+                  </div>
+              </List.Item>
+            )}
+          />
+        </div>
       </div>
-  </div>
+    </div>
   );
 };
 
