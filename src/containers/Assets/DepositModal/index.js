c2e7cfa0178588220747d45b168efceeacfea7bb
srinu391 <srinu@comdex.sg>
2022-06-16 13:11:42 +0530

    Integrate ibc deposit and withdraw.

--- src/containers/Assets/DepositModal/index.js
+++ src/containers/Assets/DepositModal/index.js
@@ -1,16 +1,138 @@
+import "./index.less";
 import * as PropTypes from "prop-types";
-import { Button, Modal, Input, Form } from "antd";
-import { Row, Col, SvgIcon } from "../../../components/common";
+import { Col, Row, SvgIcon } from "../../../components/common";
 import { connect } from "react-redux";
 import React, { useState } from "react";
+import { Button, Form, Modal, message, Spin } from "antd";
+import { aminoSignIBCTx } from "../../../services/helper";
+import { initializeIBCChain } from "../../../services/keplr";
+import { amountConversion, getAmount } from "../../../utils/coin";
 import variables from "../../../utils/variables";
-import "./index.less"
+import { denomConversion } from "../../../utils/coin";
+import { queryBalance } from "../../../services/bank/query";
+import { toDecimals, truncateString } from "../../../utils/string";
+import { fetchProofHeight } from "../../../actions/asset";
+import CustomInput from "../../../components/CustomInput";
+import { setBalanceRefresh } from "../../../actions/account";
+import { comdex } from "../../../config/network";
+import { ValidateInputNumber } from "../../../config/_validation";
+import { DEFAULT_FEE } from "../../../constants/common";
+import Snack from "../../../components/common/Snack";
 
-const DepositModal = (lang) => {
+const Deposit = ({
+  lang,
+  chain,
+  address,
+  refreshBalance,
+  setBalanceRefresh,
+}) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [sourceAddress, setSourceAddress] = useState("");
+  const [inProgress, setInProgress] = useState(false);
+  const [amount, setAmount] = useState();
+  const [availableBalance, setAvailableBalance] = useState(0);
+  const [proofHeight, setProofHeight] = useState(0);
+  const [validationError, setValidationError] = useState();
+  const [balanceInProgress, setBalanceInProgress] = useState(false);
+
+  const onChange = (value) => {
+    value = toDecimals(value).toString().trim();
+
+    setAmount(value);
+    setValidationError(
+      ValidateInputNumber(getAmount(value), availableBalance?.amount)
+    );
+  };
 
   const showModal = () => {
+    initializeIBCChain(chain.chainInfo, (error, account) => {
+      if (error) {
+        message.error(error);
+        setInProgress(false);
+        return;
+      }
+
+      setSourceAddress(account?.address);
+      setBalanceInProgress(true);
+
+      queryBalance(
+        chain?.chainInfo?.rpc,
+        account?.address,
+        chain?.denom,
+        (error, result) => {
+          setBalanceInProgress(false);
+
+          if (error) return;
+
+          setAvailableBalance(result?.balance);
+        }
+      );
+
+      fetchProofHeight(comdex?.rest, chain.sourceChannelId, (error, data) => {
+        if (error) return;
+
+        setProofHeight(data);
+      });
+    });
     setIsModalVisible(true);
+  };
+
+  const signIBCTx = () => {
+    setInProgress(true);
+
+    if (!proofHeight?.revision_height) {
+      message.error("Unable to get the latest block height");
+      setInProgress(false);
+      return;
+    }
+    const data = {
+      msg: {
+        typeUrl: "/ibc.applications.transfer.v1.MsgTransfer",
+        value: {
+          source_port: "transfer",
+          source_channel: chain.destChannelId,
+          token: {
+            denom: chain.denom,
+            amount: getAmount(amount),
+          },
+          sender: sourceAddress,
+          receiver: address,
+          timeout_height: {
+            revisionNumber: Number(proofHeight.revision_number),
+            revisionHeight: Number(proofHeight.revision_height) + 100,
+            // Need to add some blocks in order to get the timeout
+          },
+          timeout_timestamp: undefined,
+        },
+      },
+      fee: { amount: [{ denom: chain.denom, amount: "25000" }], gas: "200000" },
+      memo: "",
+    };
+
+    aminoSignIBCTx(chain.chainInfo, data, (error, result) => {
+      setInProgress(false);
+      if (error) {
+        message.error(
+          <Snack
+            message={variables[lang].tx_failed}
+            explorerUrlToTx={chain.chainInfo.explorerUrlToTx}
+            hash={result?.transactionHash}
+          />
+        );
+        return;
+      }
+
+      message.success(
+        <Snack
+          message={variables[lang].tx_success}
+          explorerUrlToTx={chain.chainInfo.explorerUrlToTx}
+          hash={result?.transactionHash}
+        />
+      );
+
+      setBalanceRefresh(refreshBalance + 1);
+      setIsModalVisible(false);
+    });
   };
 
   const handleOk = () => {
@@ -23,72 +145,127 @@ const DepositModal = (lang) => {
 
   return (
     <>
-      <Button type="primary" size="small" onClick={showModal}>Deposit</Button>
+      <Button type="primary" size="small" onClick={showModal}>
+        {variables[lang].deposit}
+      </Button>
       <Modal
-        centered={true}
         className="assets-modal"
+        centered={true}
+        closable={true}
         footer={null}
-        header={null}
         visible={isModalVisible}
-        width={550}
-        onOk={handleOk}
+        width={480}
         onCancel={handleCancel}
+        onOk={handleOk}
         closeIcon={<SvgIcon name="close" viewbox="0 0 19 19" />}
         title="IBC Deposit"
       >
-        <div className="assets-modal-inner">
-          <Form layout="vertical">
-            <Row>
-              <Col>
-                <Form.Item label="From">
-                  <Input type="text" />
-                </Form.Item>
-              </Col>
-              <SvgIcon name="arrow-right" viewbox="0 0 17.04 15.13" />
-              <Col>
-                <Form.Item label="To">
-                  <Input type="text" />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row>
-              <Col className="position-relative mt-3">
-                <div className="availabe-balance">
-                  Available
-                  <span className="ml-1">
-                    1,400 ATOM
-                  </span>
-                </div>
-                <Form.Item label="Amount to Deposit" className="assets-input-box">
-                  <Input />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row className="p-0">
-              <Col className="text-center mt-3">
-                <Button type="primary" className="btn-filled px-5" size="large" onClick={handleCancel}>
-                  Deposit
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-        </div>
+        <Form layout="vertical">
+          <Row>
+            <Col>
+              <Form.Item label="From">
+                <CustomInput
+                  type="text"
+                  value={truncateString(sourceAddress, 9, 9)}
+                  disabled
+                />
+              </Form.Item>
+            </Col>
+            <SvgIcon name="arrow-right" viewbox="0 0 17.04 15.13" />
+            <Col>
+              <Form.Item label="To">
+                <CustomInput
+                  type="text"
+                  value={truncateString(address, 9, 9)}
+                  disabled
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="position-relative mt-3">
+              <div className="availabe-balance">
+                {balanceInProgress ? (
+                  <Spin />
+                ) : (
+                  <>
+                    {variables[lang].available}
+                    <span className="ml-1">
+                      {(availableBalance &&
+                        availableBalance.amount &&
+                        amountConversion(availableBalance.amount)) ||
+                        0}{" "}
+                      {(chain.currency &&
+                        chain.currency.coinDenom &&
+                        denomConversion(chain.currency.coinDenom)) ||
+                        ""}
+                    </span>
+                    <span className="assets-maxhalf">
+                      <Button
+                        className=" active"
+                        onClick={() => {
+                          setAmount(
+                            availableBalance?.amount > DEFAULT_FEE
+                              ? amountConversion(
+                                  availableBalance?.amount - DEFAULT_FEE
+                                )
+                              : amountConversion(availableBalance?.amount)
+                          );
+                        }}
+                      >
+                        {variables[lang].max}
+                      </Button>
+                    </span>
+                  </>
+                )}
+              </div>
+              <Form.Item label="Amount to Deposit" className="assets-input-box">
+                <CustomInput
+                  value={amount}
+                  onChange={(event) => onChange(event.target.value)}
+                  validationError={validationError}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="text-center mt-2">
+              <Button
+                loading={inProgress}
+                type="primary"
+                disabled={
+                  inProgress || !Number(amount) || validationError?.message
+                }
+                className="btn-filled modal-btn"
+                onClick={signIBCTx}
+              >
+                {variables[lang].deposit}
+              </Button>
+            </Col>
+          </Row>
+        </Form>
       </Modal>
     </>
   );
 };
 
-DepositModal.propTypes = {
+Deposit.propTypes = {
   lang: PropTypes.string.isRequired,
+  refreshBalance: PropTypes.number.isRequired,
+  address: PropTypes.string,
+  chain: PropTypes.any,
 };
 
 const stateToProps = (state) => {
   return {
     lang: state.language,
+    address: state.account.address,
+    refreshBalance: state.account.refreshBalance,
   };
 };
 
 const actionsToProps = {
+  setBalanceRefresh,
 };
 
-export default connect(stateToProps, actionsToProps)(DepositModal);
+export default connect(stateToProps, actionsToProps)(Deposit);
--- src/containers/Assets/WithdrawModal/index.js
+++ src/containers/Assets/WithdrawModal/index.js
@@ -1,16 +1,126 @@
+import "./index.less";
 import * as PropTypes from "prop-types";
-import { Button, Modal, Input, Form } from "antd";
-import { Row, Col, SvgIcon } from "../../../components/common";
+import { Col, Row, SvgIcon } from "../../../components/common";
 import { connect } from "react-redux";
 import React, { useState } from "react";
+import { Button, Form, message, Modal } from "antd";
 import variables from "../../../utils/variables";
-import "./index.less"
+import { getChainConfig, initializeIBCChain } from "../../../services/keplr";
+import {
+  amountConversion,
+  denomConversion,
+  getAmount,
+} from "../../../utils/coin";
+import { defaultFee } from "../../../services/transaction";
+import { aminoSignIBCTx } from "../../../services/helper";
+import { toDecimals, truncateString } from "../../../utils/string";
+import { fetchProofHeight } from "../../../actions/asset";
+import CustomInput from "../../../components/CustomInput";
+import { setBalanceRefresh } from "../../../actions/account";
+import { ValidateInputNumber } from "../../../config/_validation";
+import Snack from "../../../components/common/Snack";
+import { comdex } from "../../../config/network";
 
-const WithdrawModal = (lang) => {
+const Withdraw = ({
+  lang,
+  chain,
+  address,
+  refreshBalance,
+  setBalanceRefresh,
+}) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [destinationAddress, setDestinationAddress] = useState("");
+  const [inProgress, setInProgress] = useState(false);
+  const [amount, setAmount] = useState();
+  const [proofHeight, setProofHeight] = useState(0);
+  const [validationError, setValidationError] = useState();
+
+  const onChange = (value) => {
+    value = toDecimals(value).toString().trim();
+
+    setAmount(value);
+    setValidationError(
+      ValidateInputNumber(getAmount(value), chain?.ibc?.amount)
+    );
+  };
 
   const showModal = () => {
+    initializeIBCChain(chain.chainInfo, (error, account) => {
+      if (error) {
+        message.error(error);
+        return;
+      }
+      setDestinationAddress(account?.address);
+      fetchProofHeight(
+        chain.chainInfo?.rest,
+        chain.sourceChannelId,
+        (error, data) => {
+          if (error) return;
+
+          setProofHeight(data);
+        }
+      );
+    });
     setIsModalVisible(true);
+  };
+
+  const signIBCTx = () => {
+    setInProgress(true);
+
+    if (!proofHeight?.revision_height) {
+      message.error("Unable to get the latest block height");
+      setInProgress(false);
+      return;
+    }
+
+    const data = {
+      msg: {
+        typeUrl: "/ibc.applications.transfer.v1.MsgTransfer",
+        value: {
+          source_port: "transfer",
+          source_channel: chain.sourceChannelId,
+          token: {
+            denom: chain.ibc?.denom,
+            amount: getAmount(amount),
+          },
+          sender: address,
+          receiver: destinationAddress,
+          timeout_height: {
+            revisionNumber: Number(proofHeight.revision_number),
+            revisionHeight: Number(proofHeight.revision_height) + 100,
+          },
+          timeout_timestamp: undefined,
+        },
+      },
+      fee: defaultFee(),
+      memo: "",
+    };
+
+    aminoSignIBCTx(getChainConfig(), data, (error, result) => {
+      setInProgress(false);
+
+      if (error) {
+        message.error(
+          <Snack
+            message={variables[lang].tx_failed}
+            explorerUrlToTx={comdex.explorerUrlToTx}
+            hash={result?.transactionHash}
+          />
+        );
+        return;
+      }
+
+      message.success(
+        <Snack
+          message={variables[lang].tx_success}
+          explorerUrlToTx={comdex.explorerUrlToTx}
+          hash={result?.transactionHash}
+        />
+      );
+
+      setBalanceRefresh(refreshBalance + 1);
+      setIsModalVisible(false);
+    });
   };
 
   const handleOk = () => {
@@ -23,72 +133,113 @@ const WithdrawModal = (lang) => {
 
   return (
     <>
-      <Button type="primary" size="small" onClick={showModal}>Withdraw</Button>
+      <Button type="primary" size="small" onClick={showModal}>
+        {variables[lang].withdraw}
+      </Button>
       <Modal
-        centered={true}
         className="assets-modal"
+        centered={true}
+        closable={true}
         footer={null}
-        header={null}
         visible={isModalVisible}
-        width={550}
-        onOk={handleOk}
+        width={480}
         onCancel={handleCancel}
+        onOk={handleOk}
         closeIcon={<SvgIcon name="close" viewbox="0 0 19 19" />}
         title="IBC Withdraw"
       >
-        <div className="assets-modal-inner">
-          <Form layout="vertical">
-            <Row>
-              <Col>
-                <Form.Item label="From">
-                  <Input type="text" />
-                </Form.Item>
-              </Col>
-              <SvgIcon name="arrow-right" viewbox="0 0 17.04 15.13" />
-              <Col>
-                <Form.Item label="To">
-                  <Input type="text" />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row>
-              <Col className="position-relative mt-3">
-                <div className="availabe-balance">
-                  Available
-                  <span className="ml-1">
-                    1,400 ATOM
-                  </span>
-                </div>
-                <Form.Item label="Amount to Deposit" className="assets-input-box">
-                  <Input />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row className="p-0">
-              <Col className="text-center mt-3">
-                <Button type="primary" className="btn-filled px-5" size="large" onClick={handleCancel}>
-                  Withdraw
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-        </div>
+        <Form layout="vertical">
+          <Row>
+            <Col>
+              <Form.Item label="From">
+                <CustomInput
+                  type="text"
+                  value={truncateString(address, 9, 9)}
+                  disabled
+                />
+              </Form.Item>
+            </Col>
+            <SvgIcon name="arrow-right" viewbox="0 0 17.04 15.13" />
+            <Col>
+              <Form.Item label="To">
+                <CustomInput
+                  type="text"
+                  value={truncateString(destinationAddress, 9, 9)}
+                  disabled
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="position-relative">
+              <div className="availabe-balance">
+                {variables[lang].available}
+                <span className="ml-1">
+                  {(chain && chain.ibc && amountConversion(chain.ibc.amount)) ||
+                    0}{" "}
+                  {(chain.currency &&
+                    chain.currency.coinDenom &&
+                    denomConversion(chain.currency.coinDenom)) ||
+                    ""}
+                </span>
+                <span className="assets-maxhalf">
+                  <Button
+                    className=" active"
+                    onClick={() => {
+                      setAmount(amountConversion(chain?.ibc?.amount || 0));
+                    }}
+                  >
+                    {variables[lang].max}
+                  </Button>
+                </span>
+              </div>
+              <Form.Item label="Amount to Withdraw">
+                <CustomInput
+                  value={amount}
+                  onChange={(event) => onChange(event.target.value)}
+                  validationError={validationError}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="text-center mt-2">
+              <Button
+                loading={inProgress}
+                type="primary"
+                disabled={
+                  inProgress || !Number(amount) || validationError?.message
+                }
+                onClick={signIBCTx}
+                className="btn-filled modal-btn"
+              >
+                {variables[lang].withdraw}
+              </Button>
+            </Col>
+          </Row>
+        </Form>
       </Modal>
     </>
   );
 };
 
-WithdrawModal.propTypes = {
+Withdraw.propTypes = {
   lang: PropTypes.string.isRequired,
+  refreshBalance: PropTypes.number.isRequired,
+  address: PropTypes.string,
+  chain: PropTypes.any,
 };
 
 const stateToProps = (state) => {
   return {
     lang: state.language,
+    address: state.account.address,
+    refreshBalance: state.account.refreshBalance,
   };
 };
 
 const actionsToProps = {
+  setBalanceRefresh,
 };
 
-export default connect(stateToProps, actionsToProps)(WithdrawModal);
+export default connect(stateToProps, actionsToProps)(Withdraw);
