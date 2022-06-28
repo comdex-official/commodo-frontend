c686b3d521a4064f33374c27fff828e82aaabada
srinu391 <srinu@comdex.sg>
2022-06-28 18:09:23 +0530

    FIx refresh balance.

--- src/components/common/Details/index.js
+++ src/components/common/Details/index.js
@@ -12,7 +12,7 @@ import { decimalConversion, marketPrice } from "../../../utils/number";
 import { DOLLAR_DECIMALS } from "../../../constants/common";
 import { connect } from "react-redux";
 
-const Details = ({ asset, poolId, markets }) => {
+const Details = ({ asset, poolId, markets, refreshBalance }) => {
   const [stats, setStats] = useState();
 
   useEffect(() => {
@@ -26,7 +26,7 @@ const Details = ({ asset, poolId, markets }) => {
         setStats(result?.AssetStats);
       });
     }
-  }, [asset, poolId]);
+  }, [asset, poolId, refreshBalance]);
 
   const data = [
     {
@@ -117,6 +117,7 @@ const Details = ({ asset, poolId, markets }) => {
 };
 
 Details.propTypes = {
+  refreshBalance: PropTypes.number.isRequired,
   asset: PropTypes.shape({
     denom: PropTypes.string,
   }),
@@ -133,6 +134,7 @@ Details.propTypes = {
 const stateToProps = (state) => {
   return {
     markets: state.oracle.market.list,
+    refreshBalance: state.account.refreshBalance,
   };
 };
 
