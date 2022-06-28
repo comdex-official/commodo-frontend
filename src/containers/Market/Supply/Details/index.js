9aeb7564c0e0f5cbfeaa74122102dddaa79ab855
srinu391 <srinu@comdex.sg>
2022-06-28 13:26:29 +0530

    Remove tabs.

--- src/containers/Market/Supply/Details/index.js
+++ src/containers/Market/Supply/Details/index.js
@@ -1,65 +1,48 @@
 import * as PropTypes from "prop-types";
 import { Col, Row } from "../../../../components/common";
 import { connect } from "react-redux";
-import {Button, message, Tabs} from "antd";
-import WithdrawTab from "./Withdraw";
-import DepositTab from "./Deposit";
+import { Button, message } from "antd";
+import Deposit from "./Deposit";
 import "./index.less";
 import { Link } from "react-router-dom";
-import {useEffect, useState} from "react";
-import {queryLendPool} from "../../../../services/lend/query";
-import {useParams} from "react-router";
-import {setPool} from "../../../../actions/lend";
+import { useEffect, useState } from "react";
+import { queryLendPool } from "../../../../services/lend/query";
+import { useParams } from "react-router";
+import { setPool } from "../../../../actions/lend";
 
-const { TabPane } = Tabs;
-
-const BackButton = {
-  right: (
-    <Link to="/market">
-      <Button className="back-btn" type="primary">
-        Back
-      </Button>
-    </Link>
-  ),
-};
-
-const SupplyDetails = ({setPool}) => {
+const SupplyDetails = ({ setPool }) => {
   const [inProgress, setInProgress] = useState(false);
 
   let { id } = useParams();
 
-  useEffect(()=>{
-    if(id){
+  useEffect(() => {
+    if (id) {
       setInProgress(true);
 
-      queryLendPool(id, (error, result)=>{
+      queryLendPool(id, (error, result) => {
         setInProgress(false);
-        if(error){
+        if (error) {
           message.error(error);
           return;
         }
-        setPool(result?.pool)
-      })
+        setPool(result?.pool);
+      });
     }
-  },[id])
+  }, [id]);
 
   return (
     <div className="app-content-wrapper">
       <Row>
-        <Col>
-          <Tabs
-            className="commodo-tabs"
-            defaultActiveKey="1"
-            tabBarExtraContent={BackButton}
-          >
-            <TabPane tab="Deposit" key="1">
-              <DepositTab dataInProgress={inProgress}/>
-            </TabPane>
-            <TabPane tab="Withdraw" key="2">
-              <WithdrawTab />
-            </TabPane>
-          </Tabs>
+        <Col className="text-right mb-3">
+          <Link to="/market">
+            <Button className="back-btn" type="primary">
+              Back
+            </Button>
+          </Link>
         </Col>
+      </Row>
+      <Row>
+        <Deposit dataInProgress={inProgress} />
       </Row>
     </div>
   );
@@ -70,7 +53,7 @@ SupplyDetails.propTypes = {
 };
 
 const actionsToProps = {
-  setPool
+  setPool,
 };
 
 export default connect(null, actionsToProps)(SupplyDetails);
