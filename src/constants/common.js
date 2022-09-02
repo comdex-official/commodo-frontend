f5d6caae6e2deb531d82e504f745a8c6243a0a6b
srinu391 <srinu@comdex.sg>
2022-09-02 15:22:07 +0530

    Add conversion.

--- src/constants/common.js
+++ src/constants/common.js
@@ -3,8 +3,8 @@ export const DEFAULT_PAGE_SIZE = 10;
 export const DEFAULT_FEE = 100000;
 export const DOLLAR_DECIMALS = 2;
 export const UC_DENOM = "uc";
-export const NUMBER_OF_TOP_ASSETS = process.env.REACT_APP_NUMBER_OF_TOP_ASSETS;
 
+export const NUMBER_OF_TOP_ASSETS = Number(process.env.REACT_APP_NUMBER_OF_TOP_ASSETS);
 export const APP_ID = process.env.REACT_APP_APP_ID;
 export const CSWAP_APP_ID = process.env.REACT_APP_CSWAP_APP_ID;
 export const HARBOR_POOL_ID_LIST = process.env.REACT_APP_HARBOR_POOL_ID_LIST;
