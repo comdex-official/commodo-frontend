3934dc9d4362c37e21069ba61b7c1b6831566a34
srinu391 <srinu@comdex.sg>
2022-11-29 09:41:11 +0530

    Update ibc denoms.

--- src/config/network.js
+++ src/config/network.js
@@ -13,8 +13,8 @@ export const comdex = {
 };
 
 export const ibcDenoms = {
-  uatom: process.env.REACT_APP_ATOM_IBC_DENOM,
-  uosmo: process.env.REACT_APP_OSMO_IBC_DENOM,
+  uatom: "uatom",
+  uosmo: "uosmo",
 };
 
 export const cmst = {
