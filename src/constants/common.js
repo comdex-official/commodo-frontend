1248924721de71523438c6063850779a076064ca
srinu391 <srinu@comdex.sg>
2023-01-10 14:11:04 +0530

    Remove env variables to seperate file.

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,3 +1,4 @@
+import { envConfig } from "../config/envConfig";
 export const DEFAULT_PAGE_NUMBER = 1;
 export const DEFAULT_PAGE_SIZE = 10;
 export const DEFAULT_BIDDING_PAGE_SIZE = 5;
@@ -5,10 +6,10 @@ export const DEFAULT_FEE = 100000;
 export const DOLLAR_DECIMALS = 2;
 export const UC_DENOM = "uc";
 
-export const NUMBER_OF_TOP_ASSETS = Number(
-  process.env.REACT_APP_NUMBER_OF_TOP_ASSETS
-);
-export const APP_ID = Number(process.env.REACT_APP_APP_ID);
-export const CSWAP_APP_ID = process.env.REACT_APP_CSWAP_APP_ID;
-export const ATOM_CMDX_POOL_ID = process.env.REACT_APP_ATOM_CMDX_POOL_ID;
+let app = process.env.REACT_APP_APP;
+
+export const NUMBER_OF_TOP_ASSETS = Number(envConfig?.[app]?.numberOfTopAssets);
+export const APP_ID = Number(envConfig?.[app]?.appId);
+export const CSWAP_APP_ID = envConfig?.[app]?.cswapAppId;
+export const ATOM_CMDX_POOL_ID = envConfig?.[app]?.atomCmdxPoolId;
 export const MAX_LTV_DEDUCTION = 0.005; // 0.5%
--- src/constants/url.js
+++ src/constants/url.js
@@ -1,7 +1,9 @@
+import { envConfig } from "../config/envConfig";
+let app = process.env.REACT_APP_APP;
 // tf = range 60- 1H, 1440 - 1D, 10080 - 1W,  43800 - 1M
 export const CAMPAIGN_URL = "https://test-campaign.comdex.one";
-export const API_URL = process.env.REACT_APP_API_URL;
-export const CSWAP_URL = process.env.REACT_APP_CSWAP_URL;
-export const REWARDS_URL = process.env.REACT_APP_REWARDS_URL;
+export const API_URL = envConfig?.apiUrl;
+export const CSWAP_URL = envConfig?.[app]?.cswapUrl;
+export const REWARDS_URL = envConfig?.[app]?.rewardsUrl;
 export const COINGECKO_API_URL =
   "https://api.coingecko.com/api/v3/simple/price?ids=comdex,cosmos,osmosis,juno-network,axlusdc,axlweth,wrapped-bitcoin,stride-staked-atom&vs_currencies=usd";
