6165e607479ea41893f7eb19188896c8f545b1f9
asif <asif.ansari7774@gmail.com>
2023-02-26 16:23:41 +0530

    card align issue

--- src/containers/Market/Supply/Details/Deposit.js
+++ src/containers/Market/Supply/Details/Deposit.js
@@ -280,32 +280,33 @@ const DepositTab = ({
                 </div>
               </div>
             </div>
-            <Row>
-              <Col sm="12" className="mt-2 mx-auto card-bottom-details">
-                <AssetStats
-                  assetId={selectedAssetId}
-                  pool={pool}
-                  parent="lend"
-                />
-              </Col>
-            </Row>
 
-            <Row className="mt-1">
-              <Col sm="12">
-                <Slider
-                  marks={marks}
-                  defaultValue={amount}
-                  value={amount}
-                  tooltip={{ open: false }}
-                  max={amountConversion(availableBalance)}
-                  onChange={handleSliderChange}
-                  className="commodo-slider market-slider-1"
-                />
-              </Col>
-            </Row>
-            <Row className="lastrow-market-dtl">
-              <Col sm="12" className="mx-auto card-bottom-details">
-                <Row className="mt-2">
+            <Row className="card-bottom-details">
+              <Col>
+                <Row>
+                  <Col sm="12" className="mt-2">
+                    <AssetStats
+                      assetId={selectedAssetId}
+                      pool={pool}
+                      parent="lend"
+                    />
+                  </Col>
+                </Row>
+
+                <Row className="mt-1">
+                  <Col sm="12">
+                    <Slider
+                      marks={marks}
+                      defaultValue={amount}
+                      value={amount}
+                      tooltip={{ open: false }}
+                      max={amountConversion(availableBalance)}
+                      onChange={handleSliderChange}
+                      className="commodo-slider market-slider-1"
+                    />
+                  </Col>
+                </Row>
+                <Row className="mt-2 lastrow-market-dtl">
                   <Col>
                     <label>Lend APY</label>
                   </Col>
