c70de5a23a71269fe4029cd5a49cca527a61217e
srinu391 <srinu@comdex.sg>
2023-02-27 16:56:05 +0530

    Integrate showing edit borrow route.

--- src/containers/Market/MarketDetails/index.js
+++ src/containers/Market/MarketDetails/index.js
@@ -1,49 +1,63 @@
 import { Tabs } from "antd";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
 import { BackButton } from "../../../components/common";
+import { decode } from "../../../utils/string";
 import BorrowDetails from "../../Market/Borrow/Details";
-import SupplyDetails from '../Supply/Details';
-import './index.less';
+import SupplyDetails from "../Supply/Details";
+import "./index.less";
 import Repay_2 from "./Repay_2";
 import Withdraw_2 from "./Withdraw_2";
 
 const PageBackButton = {
-    right: <BackButton />,
+  right: <BackButton />,
 };
 
 const MarketDetails = () => {
-    const tabItems = [
-        {
-            label: "Lend",
-            key: "1",
-            children: <SupplyDetails />
-        },
-        {
-            label: "Borrow",
-            key: "2",
-            children: <BorrowDetails />
-        },
-        {
-            label: "Withdraw",
-            key: "3",
-            children: <Withdraw_2 />
-        },
-        {
-            label: "Repay",
-            key: "4",
-            children: <Repay_2 />
-        },
-    ];
-    return (
-        <>
-            <Tabs
-                className="commodo-tabs"
-                defaultActiveKey="1"
-                tabBarExtraContent={PageBackButton}
-                items={tabItems}
-            />
-        </>
-    );
-};
+  const [activeKey, setActiveKey] = useState("1");
 
+  const location = useLocation();
+  const type = decode(location.hash);
+  useEffect(() => {
+    if (type && type === "borrow") {
+      setActiveKey("2");
+    }
+  }, []);
+
+  const tabItems = [
+    {
+      label: "Lend",
+      key: "1",
+      children: <SupplyDetails />,
+    },
+    {
+      label: "Borrow",
+      key: "2",
+      children: <BorrowDetails />,
+    },
+    {
+      label: "Withdraw",
+      key: "3",
+      children: <Withdraw_2 />,
+    },
+    {
+      label: "Repay",
+      key: "4",
+      children: <Repay_2 />,
+    },
+  ];
+  return (
+    <>
+      <Tabs
+        className="commodo-tabs"
+        defaultActiveKey="1"
+        onChange={setActiveKey}
+        activeKey={activeKey}
+        tabBarExtraContent={PageBackButton}
+        items={tabItems}
+      />
+    </>
+  );
+};
 
 export default MarketDetails;
