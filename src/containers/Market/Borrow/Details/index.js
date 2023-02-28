c52c8742fb32f59ae6c97f2f22c0bd5761849660
srinu391 <srinu@comdex.sg>
2023-02-28 17:56:58 +0530

    Add disable borrow based on lends.

--- src/containers/Market/Borrow/Details/index.js
+++ src/containers/Market/Borrow/Details/index.js
@@ -1,72 +1,12 @@
-import { message } from "antd";
-import * as PropTypes from "prop-types";
-import { useEffect, useState } from "react";
-import { connect } from "react-redux";
-import { useLocation, useParams } from "react-router";
-import { setPool, setPoolLends } from "../../../../actions/lend";
-import {
-  queryLendPool,
-  queryUserPoolLends
-} from "../../../../services/lend/query";
 import Borrow from "./Borrow";
 import "./index.less";
 
-const BorrowDetails = ({ address, setPool, setPoolLends }) => {
-  const [inProgress, setInProgress] = useState(false);
-
-  let { id } = useParams();
-
-
-  useEffect(() => {
-    if (address && id) {
-      setInProgress(true);
-
-      queryLendPool(id, (error, result) => {
-        setInProgress(false);
-        if (error) {
-          message.error(error);
-          return;
-        }
-        setPool(result?.pool);
-      });
-    }
-  }, [address, id]);
-
-  useEffect(() => {
-    if (address) {
-      queryUserPoolLends(address, (error, result) => {
-        setInProgress(false);
-        if (error) {
-          message.error(error);
-          return;
-        }
-        setPoolLends(result?.lends);
-      });
-    }
-  }, [address]);
-
+const BorrowDetails = () => {
   return (
     <div className="app-content-wrapper">
-      <Borrow dataInProgress={inProgress} />
+      <Borrow />
     </div>
   );
 };
 
-BorrowDetails.propTypes = {
-  setPool: PropTypes.func.isRequired,
-  setPoolLends: PropTypes.func.isRequired,
-  address: PropTypes.string,
-};
-
-const stateToProps = (state) => {
-  return {
-    address: state.account.address,
-  };
-};
-
-const actionsToProps = {
-  setPool,
-  setPoolLends,
-};
-
-export default connect(stateToProps, actionsToProps)(BorrowDetails);
+export default BorrowDetails;
