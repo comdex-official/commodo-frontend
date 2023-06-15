cb841126e11e1fddac3a987159977acb237a0814
Priyanshu1710 <guptapriyanshu1710@gmail.com>
2023-06-15 17:18:10 +0530

    Updated asset and auction query name and mapped stCmdx icon.

--- src/utils/SvgSpriteLoader.js
+++ src/utils/SvgSpriteLoader.js
@@ -7,7 +7,7 @@ const SvgSprite = (props) => {
   const [isErrored, setIsErrored] = useState(false);
 
   useEffect(() => {
-    const svgVersion = "0.2.9";
+    const svgVersion = "0.3.1";
     const isLocalStorage =
       "localStorage" in window && window.localStorage !== null;
     let data;
