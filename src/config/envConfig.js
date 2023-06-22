9ecae86fca4497981a9f19ed6e6e7c20ba4ccde4
svarcoder <charvai1997@gmail.com>
2023-06-22 15:20:39 +0530

    perf: fix config

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -1,44 +1,48 @@
 export const envConfig = {
-  rpc: "https://devnet.rpc.comdex.one",
-  rest: "https://devnet.rest.comdex.one",
+  // rpc: "https://test2-rpc.comdex.one",
+  // rest: "https://test2-rest.comdex.one",
+  rpc: "https://local-rpc.comdex.one",
+  rest: "https://local-rest.comdex.one",
   chainId: "test-1",
+  // chainId: "comdex-test3",
   coinDenom: "CMDX",
   coinMinimalDenom: "ucmdx",
   coinDecimals: 6,
   prefix: "comdex",
   coinType: 118,
   chainName: "Comdex Test Chain",
-  explorerUrlToTx: "https://dev-explorer.comdex.one/transactions/{txHash}",
-  apiUrl: "https://test-stat.comdex.one",
+  explorerUrlToTx: "https://test2-explorer.comdex.one/transactions/{txHash}",
+  // apiUrl: "https://testnet-stat.comdex.one",
+  apiUrl: "http://95.217.185.27:8000",
   comdexStakingUrl: "https://comdex.omniflix.co/stake",
-  webSocketApiUrl: "wss://devnet.rpc.comdex.one/websocket",
+  webSocketApiUrl: "wss://testnet2rpc.comdex.one/websocket",
   symbol: "CMDX",
-
   commodo: {
     appId: 3,
-    cswapAppId: 2,
+    cswapAppId: 1,
     numberOfTopAssets: 3,
-    websiteUrl: "https://devnet.commodo.one",
-    cswapUrl: "https://devnet.cswap.one",
+    websiteUrl: "https://testnet.commodo.one",
+    cswapUrl: "https://staging.cswap.one",
     rewardsUrl: "https://docs.commodo.one/rewards",
     atomCmdxPoolId: 1,
   },
-
   cSwap: {
-    appId: 2,
+    appId: 1,
     masterPoolId: 1,
-    websiteUrl: "https://devnet.cswap.one",
-    networkTag: "Devnet",
+    websiteUrl: "https://testnet.cswap.one",
+    networkTag: "testnet2",
   },
-
   harbor: {
     title: "Harbor Protocol",
-    websiteUrl: "https://devnet.harborprotocol.one",
-    appId: 1,
-    governanceContractAddress: "comdex17p9rzwnnfxcjp32un9ug7yhhzgtkhvl9jfksztgw5uh69wac2pgs4jg6dx",
-    lockingContractAddress: "comdex1nc5tatafv6eyq7llkr2gv50ff9e22mnf70qgjlv737ktmt4eswrqdfklyz",
-    airdropContractAddress: "comdex1ghd753shjuwexxywmgs4xz7x2q732vcnkm6h2pyv9s6ah3hylvrqfy9rd8",
+    websiteUrl: "https://testnet.harborprotocol.one",
+    appId: 2,
+    governanceContractAddress:
+      "comdex1qwlgtx52gsdu7dtp0cekka5zehdl0uj3fhp9acg325fvgs8jdzksjvgq6q",
+    lockingContractAddress:
+      "comdex1ghd753shjuwexxywmgs4xz7x2q732vcnkm6h2pyv9s6ah3hylvrqfy9rd8",
+    airdropContractAddress:
+      "comdex1gya5c9y6yehyps5kz9vt2dptgkncvvx76mf05jext4axh9x6vpxql3vu9v",
     harborAirdropApiUrl: "http://3.7.255.161",
-    harborDashboardTVLApiUrl: " https://test-stat.comdex.one"
+    harborDashboardTVLApiUrl: "https://testnet-stat.comdex.one",
   },
 };
