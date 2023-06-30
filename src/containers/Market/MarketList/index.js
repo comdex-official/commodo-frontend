3be9f5bd49e767ed26a700359bb333b9b25e4ddb
svarcoder <charvai1997@gmail.com>
2023-06-30 12:05:29 +0530

    perf: fixed c-pool

--- src/containers/Market/MarketList/index.js
+++ src/containers/Market/MarketList/index.js
@@ -116,7 +116,7 @@ const MarketList = ({ assetMap, setPools, pools, lendPools, userLendList }) => {
           {pools?.length > 0 ? (
             pools &&
             pools?.map((item) => {
-              // if (Number(item.poolId) === 1) return;
+              if (Number(item.poolId) === 1) return;
               return (
                 <div
                   className="market-list-item"
