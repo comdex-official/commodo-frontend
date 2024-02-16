5f5d225fbac57572bedceecb7fbc2d74aad06104
“svarcoder” <charvai1997@gmail.com>
2024-02-16 14:27:07 +0530

    perf: gas increase

--- src/services/transaction.js
+++ src/services/transaction.js
@@ -64,7 +64,7 @@ export const abbreviateMessage = (msg) => {
 export const defaultFee = () => {
   return {
     amount: [{ denom: "ucmdx", amount: DEFAULT_FEE.toString() }],
-    gas: "200000",
+    gas: "250000",
   };
 };
 
